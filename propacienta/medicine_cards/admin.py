f9d597fad286b41e4f188720c3b5991be8a2bf00
ghostforpy <ghostformobile@gmail.com>
2022-02-06 23:58:32 +0300

    add medicine card model, makemigrations

--- propacienta/medicine_cards/admin.py
+++ propacienta/medicine_cards/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import MedicineCard
 # Register your models here.
+
+@admin.register(MedicineCard)
+class PacientAdmin(admin.ModelAdmin):
+    list_display = ("user_name",)
+    #inlines = [UserModelAdmin, MedicineCardModelAdmin]
+
+    def user_name(self, obj):
+        return obj.pacient.__str__()
--- propacienta/medicine_cards/models.py
+++ propacienta/medicine_cards/models.py
@@ -1,3 +1,20 @@
+from statistics import mode
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class MedicineCard(models.Model):
+    pacient = models.OneToOneField(
+        "pacients.pacient",
+        on_delete=models.CASCADE,
+        related_name="medicine_card",
+        verbose_name=_("Medicine card")
+    )
+    height = models.DecimalField(_("Height"), max_digits=8, decimal_places=2, null=True)
+    weight = models.DecimalField(_("Weight"), max_digits=8, decimal_places=2, null=True)
+    average_pressure = models.CharField(max_length=20, null=True)
+
+    class Meta:
+        verbose_name = "Медицинская карта"
+        verbose_name_plural = "Медицинские карты"
