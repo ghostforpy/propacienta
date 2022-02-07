5ffce2dc2e2d6bac8b8e2db3dfe7351d5a6dc4fc
ghostforpy <ghostformobile@gmail.com>
2022-02-07 23:27:25 +0300

    init medicines app

---
+++ propacienta/medicines/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class Medicine(models.Model):
+    """Medicine model."""
+    title = models.CharField(
+        _("Наименование"),
+        max_length=250,
+        unique=True
+        )
+    
+    class Meta:
+        verbose_name = "Медикамент"
+        verbose_name_plural = "Медикаменты"
+
+    def __str__(self) -> str:
+        return self.title
