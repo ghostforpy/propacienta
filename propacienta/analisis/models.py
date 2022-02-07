87ed1a4c124d003845b1b37b27b66622e5c0356a
ghostforpy <ghostformobile@gmail.com>
2022-02-07 23:35:26 +0300

    add Analysis app, makemigrations

--- propacienta/analisis/models.py
+++ propacienta/analisis/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Analysis(models.Model):
+    """Analysis model."""
+    title = models.CharField(
+        _("Наименование"),
+        max_length=250,
+        unique=True
+        )
+    
+    class Meta:
+        verbose_name = "Анализ"
+        verbose_name_plural = "Анализы"
+
+    def __str__(self) -> str:
+        return self.title
