7a2b7b060a3ba8767b0f63a2364904fcf9ea5e5d
ghostforpy <ghostformobile@gmail.com>
2022-02-07 22:34:36 +0300

    edit hospitals app

--- propacienta/hospitals/admin.py
+++ propacienta/hospitals/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Hospital
 # Register your models here.
+
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ("title", "town", "address")
+    search_fields = ["title", "town__title", "address"]
+    list_filter = ("town",)
+    list_select_related = ["town"]
--- propacienta/hospitals/models.py
+++ propacienta/hospitals/models.py
@@ -1,3 +1,25 @@
+from pyexpat import model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class HospitalTown(models.Model):
+    title = models.CharField(_("Наименование"), unique=True, max_length=150)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Hospital(models.Model):
+    title = models.CharField(_("Наименование"), unique=True, max_length=150)
+    town = models.ForeignKey(
+        HospitalTown,
+        verbose_name=_("Населенный пункт"),
+        on_delete=models.DO_NOTHING,
+        related_name="hospitals"
+    )
+    address = models.CharField(_("Адрес"), max_length=250)
+
+    def __str__(self) -> str:
+        return self.title
