4915a4866bcae15e3cda807991b619240e739d7a
ghostforpy <ghostformobile@gmail.com>
2022-02-07 23:14:50 +0300

    add doctors specializations and subspecializations

--- propacienta/doctors/models.py
+++ propacienta/doctors/models.py
@@ -3,6 +3,40 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
+class DoctorSpecialization(models.Model):
+    title = models.CharField(
+        _("Специализация"),
+        max_length=250,
+        unique=True
+        )
+    
+    class Meta:
+        verbose_name = "Специализация"
+        verbose_name_plural = "Специализации"
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class DoctorSubSpecialization(models.Model):
+    title = models.CharField(
+        _("Узкая специализация"),
+        max_length=250,
+        unique=True
+        )
+    specialization = models.ForeignKey(
+        DoctorSpecialization,
+        on_delete=models.CASCADE,
+        related_name="sub_specializations"
+        )
+    
+    class Meta:
+        verbose_name = "Узкая специализация"
+        verbose_name_plural = "Узкие специализации"
+
+    def __str__(self) -> str:
+        return self.title
+
 
 class Doctor(models.Model):
     """Model for doctors."""
@@ -13,7 +47,15 @@ class Doctor(models.Model):
         verbose_name=_("Клиники"),
         related_name="doctors"
     )
-    
+    specializations = models.ManyToManyField(
+        DoctorSpecialization,
+        related_name="doctors"
+    )
+    sub_specializations = models.ManyToManyField(
+        DoctorSubSpecialization,
+        related_name="doctors"
+    )
+
     class Meta:
         verbose_name = "Врач"
         verbose_name_plural = "Врачи"
