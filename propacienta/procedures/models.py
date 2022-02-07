83872dd10ffa734bfe6564e5a4b5e9661ff55b0d
ghostforpy <ghostformobile@gmail.com>
2022-02-07 23:33:11 +0300

    add procedure model, makemigrations

--- propacienta/procedures/models.py
+++ propacienta/procedures/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Procedure(models.Model):
+    """Procedure model."""
+    title = models.CharField(
+        _("Наименование"),
+        max_length=250,
+        unique=True
+        )
+    
+    class Meta:
+        verbose_name = "Процедура"
+        verbose_name_plural = "Процедуры"
+
+    def __str__(self) -> str:
+        return self.title
