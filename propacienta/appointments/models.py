73ee64da9ae488c63a9f857c9491bcc523737efa
ghostforpy <ghostformobile@gmail.com>
2022-02-08 23:26:55 +0300

    add appointment app models, makemigrations

--- propacienta/appointments/models.py
+++ propacienta/appointments/models.py
@@ -52,3 +52,69 @@ class AppointmentOrder(models.Model):
 
     def __str__(self) -> str:
         return "{} к {}".format(self.pacient, self.doctor)
+
+
+class AppointmentSurvey(models.Model):
+    """Модель опросника для пациента перед приемом у врача."""
+    appointment_order = models.OneToOneField(
+        AppointmentOrder,
+        on_delete=models.DO_NOTHING,
+        related_name="survey"
+    )
+    complaints = models.TextField(_("Жалобы"))
+    complaints_date = models.DateField(_("Когда впервые появились жалобы"))
+    treatment = models.TextField(_("Лечение"))
+
+
+class DoctorAppointment(models.Model):
+    """Модель факта проеведения приема у врача."""
+    pacient = models.ForeignKey(
+        "pacients.pacient",
+        on_delete=models.DO_NOTHING,
+        related_name="doctor_appointments"
+    )
+    doctor = models.ForeignKey(
+        "doctors.doctor",
+        on_delete=models.DO_NOTHING,
+        related_name="doctor_appointments"
+    )
+    dt = models.DateTimeField(_("Фактическое время приема"))
+    appointment_order = models.OneToOneField(
+        AppointmentOrder,
+        on_delete=models.DO_NOTHING,
+        related_name="doctor_appointment",
+        blank=True,
+        null=True
+    )
+    anamnesis = models.TextField(_("Анамнез"))
+    medicine_card = models.ForeignKey(
+        "medicine_cards.medicinecard",
+        on_delete=models.DO_NOTHING,
+        related_name="doctor_appointments"
+    )
+    doctor_specialization = models.ForeignKey(
+        "doctors.doctorspecialization",
+        on_delete=models.DO_NOTHING,
+        related_name="doctor_appointments",
+        null=True,
+        blank=True
+    )
+    doctor_sub_specialization = models.ForeignKey(
+        "doctors.doctorsubspecialization",
+        on_delete=models.DO_NOTHING,
+        related_name="doctor_appointments",
+        null=True,
+        blank=True
+    )
+    complaints = models.TextField(_("Жалобы"))
+    complaints_date = models.DateField(_("Когда впервые появились жалобы"))
+    treatment = models.TextField(_("Лечение"))
+    rating_for_pacient = models.FloatField(_("Оценка пациенту"))
+    rating_for_doctor = models.FloatField(_("Оценка доктору"))
+
+    class Meta:
+        verbose_name = "Приём врача"
+        verbose_name_plural = "Приёмы врачей"
+
+    def __str__(self) -> str:
+        return "{} к {}".format(self.pacient, self.doctor)
