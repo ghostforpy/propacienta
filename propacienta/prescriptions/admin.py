c127757c3dd24f01fd951eabcd1d2822a5067155
ghostforpy <ghostformobile@gmail.com>
2022-02-09 23:25:33 +0300

    edit app prescriptions

--- propacienta/prescriptions/admin.py
+++ propacienta/prescriptions/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import MedicinePrescription, ProcedurePrescription, AnalisisPrescription
 # Register your models here.
+
+
+class BasePrescriptionAdmin(admin.ModelAdmin):
+    list_display = ("pacient", "doctor",)
+    list_select_related = True
+
+    def pacient(self, obj):
+        return obj.medicine_card.pacient
+
+
+@admin.register(MedicinePrescription)
+class MedicinePrescriptionAdmin(BasePrescriptionAdmin):
+    pass
+
+
+@admin.register(ProcedurePrescription)
+class ProcedurePrescriptionAdmin(BasePrescriptionAdmin):
+    pass
+
+
+@admin.register(AnalisisPrescription)
+class AnalisisPrescriptionAdmin(BasePrescriptionAdmin):
+    pass
--- propacienta/prescriptions/models.py
+++ propacienta/prescriptions/models.py
@@ -1,3 +1,96 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class BasePrescription(models.Model):
+    """Абстрактная базовая модель назначения врача."""
+    medicine_card = models.ForeignKey(
+        "medicine_cards.medicinecard",
+        on_delete=models.DO_NOTHING,
+        related_name="%(class)s",
+    )
+    doctor = models.ForeignKey(
+        "doctors.doctor",
+        on_delete=models.DO_NOTHING,
+        related_name="%(class)s",
+    )
+    doctor_appointment = models.ForeignKey(
+        "appointments.doctorappointment",
+        on_delete=models.DO_NOTHING,
+        related_name="%(class)s",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Prescription(BasePrescription):
+    """Абстрактная модель назначения врача."""
+    duration = models.PositiveIntegerField(_("Продолжительность"))
+    rate = models.PositiveIntegerField(_("Частота"))
+    number = models.FloatField(_("Количество"))
+    per = models.CharField(
+        _("Единица измерения частоты"),
+        help_text="День, неделя, месяц, час",
+        max_length=20
+    )
+
+    class Meta:
+        abstract = True
+
+
+class MedicinePrescription(Prescription):
+    """Модель назначения медикаментов."""
+    medicine = models.ForeignKey(
+        "medicines.medicine",
+        on_delete=models.DO_NOTHING,
+        related_name="medicine_prescriptions"
+    )
+
+    class Meta:
+        verbose_name = "Назначение медикаментов"
+        verbose_name_plural = "Назначения медикаментов"
+
+    def __str__(self) -> str:
+        return "{} для {}".format(
+            self.Meta.verbose_name,
+            self.medicine_card.pacient
+            )
+
+
+class ProcedurePrescription(Prescription):
+    """Модель назначения процедур."""
+    procedure = models.ForeignKey(
+        "procedures.procedure",
+        on_delete=models.DO_NOTHING,
+        related_name="procedure_prescriptions"
+    )
+
+    class Meta:
+        verbose_name = "Назначение процедуры"
+        verbose_name_plural = "Назначения процедур"
+
+    def __str__(self) -> str:
+        return "{} для {}".format(
+            self.Meta.verbose_name,
+            self.medicine_card.pacient
+            )
+
+
+class AnalisisPrescription(BasePrescription):
+    """Модель назначения анализа."""
+    analisis = models.ForeignKey(
+        "analisis.analysis",
+        on_delete=models.DO_NOTHING,
+        related_name="analisis_prescriptions"
+    )
+
+    class Meta:
+        verbose_name = "Назначение анализа"
+        verbose_name_plural = "Назначения анализов"
+
+    def __str__(self) -> str:
+        return "{} для {}".format(
+            self.Meta.verbose_name,
+            self.medicine_card.pacient
+            )
