4a0d732b1849d67f2c9f94fde1e3ac9aedbab68f
ghostforpy <ghostformobile@gmail.com>
2022-02-13 17:55:08 +0300

    edit admin panel app pacients

--- propacienta/pacients/admin.py
+++ propacienta/pacients/admin.py
@@ -1,8 +1,59 @@
+from select import select
 from django.contrib import admin
-from .models import Pacient
+from django.urls import reverse
+from django.utils.encoding import force_text
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from medicine_cards.models import MedicineCard
+from operations.models import TransferredOperation
+from prescriptions.models import ProcedurePrescription, AnalisisPrescription, MedicinePrescription
+from diseases.models import ChronicDisease, TransferredDisease
+from .models import Pacient
+
+
+class EditMixin:
+    extra = 0
+    readonly_fields = ('get_edit_link',)
+    def get_edit_link(self, obj=None):
+        if obj.pk:  # if object has already been saved and has a primary key, show link to it
+            url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[force_text(obj.pk)])
+            return """<a href="{url}">{text}</a>""".format(
+                url=url,
+                text=_("Edit this %s separately") % obj._meta.verbose_name,
+            )
+        return _("(save and continue editing to create a link)")
+    get_edit_link.short_description = _("Просмотреть и изменить")
+    get_edit_link.allow_tags = True
+
+
+class TransferredOperationAdmin(EditMixin, admin.StackedInline):
+    model = TransferredOperation
+    fields = ['get_edit_link', "operation", "medicine_card", "effect", "date"]
+
+
+class ChronicDiseaseAdmin(EditMixin, admin.StackedInline):
+    model = ChronicDisease
+    fields = ['get_edit_link', "disease", "medicine_card", "treatment"]
+
+class TransferredDiseaseAdmin(EditMixin, admin.StackedInline):
+    model = TransferredDisease
+    fields = ['get_edit_link', "disease", "medicine_card",
+                "diagnosis", "diagnosis_date", "diagnosis_year",
+                "treatment_date", "treatment_end_date"]
+
+class ProcedurePrescriptionAdmin(EditMixin, admin.StackedInline):
+    model = ProcedurePrescription
+    fields = ['get_edit_link', "procedure", "medicine_card", "doctor", "doctor_appointment"]
+
+
+class AnalisisPrescriptionAdmin(EditMixin, admin.StackedInline):
+    model = AnalisisPrescription
+    fields = ['get_edit_link', "analisis", "medicine_card", "doctor", "doctor_appointment"]
+
+class MedicinePrescriptionAdmin(EditMixin, admin.StackedInline):
+    model = MedicinePrescription
+    fields = ['get_edit_link', "medicine", "medicine_card", "doctor", "doctor_appointment"]
+
 
 class UserModelAdmin(admin.StackedInline):
     model = get_user_model()
@@ -26,7 +77,17 @@ class MedicineCardModelAdmin(admin.StackedInline):
 @admin.register(Pacient)
 class PacientAdmin(admin.ModelAdmin):
     list_display = ("user_name", "phone",)
-    inlines = [UserModelAdmin, MedicineCardModelAdmin]
+    list_select_related = True
+    inlines = [
+        UserModelAdmin,
+        MedicineCardModelAdmin,
+        TransferredOperationAdmin,
+        ChronicDiseaseAdmin,
+        TransferredDiseaseAdmin,
+        ProcedurePrescriptionAdmin,
+        AnalisisPrescriptionAdmin,
+        MedicinePrescriptionAdmin
+        ]
 
     def user_name(self, obj):
         return obj.__str__()
