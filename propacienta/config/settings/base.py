97449dba1881dc2a2286d3b76d2ec2f40aa668cc
ghostforpy <ghostformobile@gmail.com>
2022-02-21 14:24:17 +0300

    disable token auth

--- propacienta/config/settings/base.py
+++ propacienta/config/settings/base.py
@@ -318,7 +318,7 @@ SOCIALACCOUNT_FORMS = {"signup": "propacienta.users.forms.UserSocialSignupForm"}
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        #"rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
