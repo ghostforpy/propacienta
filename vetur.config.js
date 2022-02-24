049848397793a104af667bf11884c6276f49ad0b
ghostforpy <ghostformobile@gmail.com>
2022-02-24 22:46:50 +0300

    add vetur config

---
+++ vetur.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    projects: [
+        {
+            root: './propacienta/frontend/propacienta', // root of your vue project (should contain package.json)
+            package: './package.json', // Relative to root property, don't change this.
+            tsconfig: './tsconfig.json',  // Relative to root property, don't change this.
+        }
+    ]
+}
