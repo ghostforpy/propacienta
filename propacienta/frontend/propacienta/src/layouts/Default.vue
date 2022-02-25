762d25dabb4c79e15c943bb7fcbf126e4f67b641
ghostforpy <ghostformobile@gmail.com>
2022-02-25 21:38:07 +0300

    def loyout

---
+++ propacienta/frontend/propacienta/src/layouts/Default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-app-bar app color="blue">
+      <v-app-bar-title>
+        <router-link class="link" to="/">Propacienta</router-link>
+      </v-app-bar-title>
+    </v-app-bar>
+    <section class="content">
+      <!--Элемент, который при отрисовке будет заменен на ваш view-->
+      <slot />
+    </section>
+    <footer></footer>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: "DefaultLayout",
+};
+</script>
