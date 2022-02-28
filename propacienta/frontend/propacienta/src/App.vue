3b358e6ae187e902cca26ae308f7d8cf8ccefa31
ghostforpy <ghostformobile@gmail.com>
2022-02-28 22:53:14 +0300

    .

--- propacienta/frontend/propacienta/src/App.vue
+++ propacienta/frontend/propacienta/src/App.vue
@@ -108,7 +108,7 @@
           {{ link }}
         </v-btn>
         <v-col class="cyan lighten-2 py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          {{ new Date().getFullYear() }} — <strong>Propacienta</strong>
         </v-col>
       </v-row>
     </v-footer>
