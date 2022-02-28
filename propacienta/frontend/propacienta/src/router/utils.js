ff0152cbf9b98db5a0293d0d2044e9ef88f5326c
ghostforpy <ghostformobile@gmail.com>
2022-02-28 22:52:29 +0300

    add router utils

---
+++ propacienta/frontend/propacienta/src/router/utils.js
@@ -0,0 +1,37 @@
+import store from '@/store'
+import { AUTH_PING } from "@/store/actions/auth";
+
+export const ifAuthenticated = async (to, from, next) => {
+    if (store.getters.isAuthenticated === '') {
+        const isAuth = await store.dispatch(AUTH_PING)
+        if (isAuth) {
+            next()
+            return
+        }
+        next({ name: 'login', query: { redirect: to.fullPath } })
+        return
+    }
+
+    if (store.getters.isAuthenticated) {
+        next()
+        return
+    }
+    next({ name: 'login', query: { redirect: to.fullPath } })
+}
+
+export const ifNotAuthenticated = async (to, from, next) => {
+    if (store.getters.isAuthenticated === '') {
+        const isAuth = await store.dispatch(AUTH_PING)
+        if (isAuth) {
+            next({ name: 'logout' })
+            return
+        }
+        next()
+        return
+    }
+    if (store.getters.isAuthenticated) {
+        next({ name: 'logout' })
+        return
+    }
+    next()
+}
