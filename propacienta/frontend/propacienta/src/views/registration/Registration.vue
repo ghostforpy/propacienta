0238ab9a1455131a68a35208deb5da40f7205874
ghostforpy <ghostformobile@gmail.com>
2022-03-07 18:29:54 +0300

    registration view frontend edit

--- propacienta/frontend/propacienta/src/views/registration/Registration.vue
+++ propacienta/frontend/propacienta/src/views/registration/Registration.vue
@@ -13,6 +13,16 @@
                 label="Адрес электронной почты"
                 type="text"
                 v-model="email"
+                ref="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                name="phone_pacient"
+                label="Телефон"
+                type="text"
+                v-model="phone_pacient"
+                ref="phone_pacient"
+                :rules="phoneRules"
               ></v-text-field>
               <v-text-field
                 id="password"
@@ -20,6 +30,10 @@
                 label="Пароль"
                 type="password"
                 v-model="password"
+                ref="password"
+                :rules="[
+                  (value) => !!value || 'Это поле является обязательным.',
+                ]"
               ></v-text-field>
               <v-text-field
                 id="re_password"
@@ -27,23 +41,29 @@
                 label="Повторите пароль"
                 type="password"
                 v-model="re_password"
+                ref="re_password"
+                :rules="[
+                  (value) => !!value || 'Это поле является обязательным.',
+                ]"
               ></v-text-field>
               <v-checkbox
                 v-model="role_doctor"
                 label="Регистрация аккаунта врача"
               ></v-checkbox>
-              <!-- <v-text-field
-                name="first_name"
-                label="Имя"
-                type="text"
-                v-model="first_name"
-              ></v-text-field>
+              <v-checkbox
+                v-model="same_phone"
+                v-if="role_doctor"
+                label="Телефон соответствует основному"
+              ></v-checkbox>
               <v-text-field
-                name="last_name"
-                label="Фамилия"
+                v-show="role_doctor && !same_phone"
+                name="phone_doctor"
+                label="Телефон врача"
                 type="text"
-                v-model="last_name"
-              ></v-text-field> -->
+                v-model="phone_doctor"
+                ref="phone_doctor"
+                :rules="phoneRules"
+              ></v-text-field>
             </v-form>
             <div v-if="registrationError" v-bind:class="{ shake: animError }">
               <v-alert transition="fade-transition" type="error">
@@ -63,12 +83,21 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
+              block
               color="cyan"
+              class="white-content"
               v-on:click="onSubmit"
               v-if="!registrationSuccess"
-              >Зарегистрироваться</v-btn
+              ><span v-if="!loading">Зарегистрироваться</span
+              ><v-progress-circular
+                v-else
+                indeterminate
+                color="primary"
+              ></v-progress-circular
+            ></v-btn>
+            <v-btn color="cyan" class="white-content" v-on:click="onOk" v-else
+              >Ok</v-btn
             >
-            <v-btn color="cyan" v-on:click="onOk" v-else>Ok</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -90,12 +119,32 @@ export default {
   data: function () {
     return {
       email: null,
+      phone_pacient: null,
       password: null,
       re_password: null,
       role_doctor: false,
       registrationSuccess: false,
       registrationError: false,
       animError: false,
+      loading: false,
+      phone_doctor: null,
+      same_phone: false,
+      submitAvailableSt: false,
+      phoneRules: [
+        (value) => !!value || "Это поле является обязательным.",
+        (value) => {
+          const pattern = /^[+]*[0-9]{11}$/g;
+          return pattern.test(value) || "Неправильный формат номера.";
+        },
+      ],
+      emailRules: [
+        (value) => !!value || "Это поле является обязательным.",
+        (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Неправильный формат номера.";
+        },
+      ],
     };
   },
   computed: {
@@ -103,7 +152,8 @@ export default {
       if (this.password != this.re_password) {
         return "Два пароля не совпадают.";
       }
-      return `${this.$store.getters.registrationErrorEmailState} ${this.$store.getters.registrationErrorPasswordState}`;
+      return `${this.$store.getters.registrationErrorState} ${this.$store.getters.registrationErrorEmailState} 
+      ${this.$store.getters.registrationErrorPasswordState} ${this.$store.getters.registrationErrorPhoneState}`;
     },
   },
   methods: {
@@ -112,7 +162,32 @@ export default {
         path: "/",
       });
     },
+    submitAvailable: function () {
+      return (
+        this.email != null &&
+        this.password != null &&
+        this.re_password != null &&
+        this.phone_pacient != null &&
+        this.$refs.email.valid &&
+        this.$refs.password.valid &&
+        this.$refs.re_password.valid &&
+        this.$refs.phone_pacient.valid &&
+        this.password === this.re_password &&
+        !(
+          this.role_doctor &&
+          !this.same_phone &&
+          !this.$refs.phone_doctor.valid
+        )
+      );
+    },
     onSubmit: async function () {
+      if (!this.submitAvailable()) {
+        if (this.registrationError) {
+          this.animError = true;
+        }
+        this.registrationError = true;
+        return;
+      }
       if (this.password != this.re_password) {
         if (this.registrationError) {
           this.animError = true;
@@ -122,14 +197,19 @@ export default {
       }
       let data = {
         email: this.email,
+        phone_pacient: this.phone_pacient,
         password: this.password,
         re_password: this.re_password,
         role_doctor: this.role_doctor,
+        phone_doctor:
+          this.role_doctor && this.same_phone
+            ? this.phone_pacient
+            : this.phone_doctor,
       };
       this.animError = false;
-
+      this.loading = true;
       const res = await this.$store.dispatch(REGISTRATION_REQUEST, data);
-
+      this.loading = false;
       if (res) {
         this.registrationError = false;
         this.registrationSuccess = true;
@@ -145,7 +225,7 @@ export default {
 </script>
 
 <style>
-.v-btn__content {
+.white-content.v-btn {
   color: white;
 }
 /* .shake {
