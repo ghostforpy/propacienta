85c126d2e84e3c0d9b172570790f7fd5fbd7e7a5
ghostforpy <ghostformobile@gmail.com>
2022-03-28 23:19:25 +0300

    add private folder diseases

--- propacienta/diseases/utils.py
+++ propacienta/diseases/utils.py
@@ -1,2 +1,2 @@
 def discharge_epicrisis_dir(instance, filename: str) -> str:
-    return "discharge_epicrisis/pacient_%s/%s" % (instance.discharge_epicris.pacient.id, filename)
+    return "private/discharge_epicrisis/pacient_%s/%s" % (instance.discharge_epicris.pacient.id, filename)
