44a09b9d840acaee99372e8feae491bfb8ef21ce
ghostforpy <ghostformobile@gmail.com>
2022-04-13 23:42:40 +0300

    deny permission add

---
+++ propacienta/propacienta/users/utils.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+
+class DenyAll(BasePermission):
+    """
+    Object-level permission to only allow requests by active doctors.
+    """
+    def has_permission(self, request, view):
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        return False
