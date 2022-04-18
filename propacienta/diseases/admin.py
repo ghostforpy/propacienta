5bb77993e2380cb157918749a254f739c18ca905
ghostforpy <ghostformobile@gmail.com>
2022-04-18 11:32:44 +0300

    edit admin panel DischargeEpicrisTabularAdmin

--- propacienta/diseases/admin.py
+++ propacienta/diseases/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.encoding import force_text
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
 from .models import (
     ChronicDisease,
@@ -8,6 +12,28 @@ from .models import (
     Disease,
     TransferredDisease,
 )
+
+
+class EditMixin:
+    extra = 0
+    readonly_fields = ('get_edit_link',)
+
+    def get_edit_link(self, obj=None):
+        if obj.pk:  # if object has already been saved and has a primary key, show link to it
+            url = reverse(
+                'admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name),
+                args=[force_text(obj.pk)]
+            )
+            return format_html(
+                '<a href="{url}">{text}</a>',
+                url=url,
+                # text=_("%s") % obj.__str__()
+                text="Перейти на страницу"
+            )
+
+        return _("(save and continue editing to create a link)")
+    get_edit_link.short_description = _("Просмотреть и изменить")
+    get_edit_link.allow_tags = True
 
 
 @admin.register(Disease)
@@ -35,8 +61,14 @@ class DischargeEpicrisAdmin(admin.ModelAdmin):
                     "pacient__phone",)
 
 
-class DischargeEpicrisTabularAdmin(admin.TabularInline):
+class DischargeEpicrisTabularAdmin(EditMixin, admin.StackedInline):
     model = DischargeEpicris
+    fields = ["get_edit_link", "chronic_disease", "transferred_disease", "pacient", "d", "epicris"]
+
+    # def get_fieldsets(self, request, obj=None):
+    #     fieldsets = super(DischargeEpicrisTabularAdmin, self).get_fieldsets(request, obj)
+    #     fieldsets[0][1]['fields'] += ['get_edit_link'] 
+    #     return fieldsets
 
 
 @admin.register(ChronicDisease)
@@ -54,7 +86,7 @@ class ChronicDiseaseAdmin(admin.ModelAdmin):
 class TransferredDiseaseAdmin(admin.ModelAdmin):
     list_display = ("pacient", "disease")
     list_select_related = True
-    #inlines = [DischargeEpicrisTabularAdmin]
+    inlines = [DischargeEpicrisTabularAdmin]
     list_filter = ("disease",)
     search_fields = ("pacient__user__email", "pacient__user__last_name",
                     "pacient__user__first_name", "pacient__user__patronymic",
