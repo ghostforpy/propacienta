1b0fe742f9b20c61d7d1a5c11bc837ccaa65c0a9
ghostforpy <ghostformobile@gmail.com>
2022-07-26 23:38:34 +0300

    fix

--- propacienta/doctors/utils.py
+++ propacienta/doctors/utils.py
@@ -9,7 +9,7 @@ def request_by_doctor(request):
     if request.user != AnonymousUser:
         if IS_DOCTOR_HEADER in request.headers:
             if request.headers.get(IS_DOCTOR_HEADER) == IS_DOCTOR_HEADER_STATUS:
-                if request.user.doctor != None:
+                if request.user.doctor is not None:
                     if request.user.doctor.is_active:
                         return request.user.doctor
     return None
