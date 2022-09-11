0c3162d0d9f8a668c90c8c0b78d0e892e04be598
ghostforpy <ghostformobile@gmail.com>
2022-09-11 16:05:29 +0300

    edit BASE_URL

--- propacienta/frontend/propacienta/src/api/HTTP.js
+++ propacienta/frontend/propacienta/src/api/HTTP.js
@@ -1,9 +1,10 @@
 //let url = document.location.protocol + '//' + document.location.host
 import axios from "axios";
-export const BASE_URL = '0.0.0.0';
+// export const BASE_URL = '0.0.0.0';
+export const BASE_URL = 'localhost';
 const HTTP = axios.create({
     //baseURL: 'http://0.0.0.0:8000/api/',
-    baseURL: 'http://0.0.0.0/',
+    baseURL: 'http://localhost/',
     //baseURL: url + '/api/',
     headers: {
         // 'IsDoctor': true
