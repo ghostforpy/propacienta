cdab57907b55b34661e6c511555323a2967080e7
ghostforpy <ghostformobile@gmail.com>
2022-09-21 21:29:45 +0300

    add WebDial model, makemigrations, edit admin panel

--- propacienta/webdials/admin.py
+++ propacienta/webdials/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import WebDial
+
 # Register your models here.
+
+
+@admin.register(WebDial)
+class WebDialAdmin(admin.ModelAdmin):
+    search_fields = (
+        "initiator__email",
+        "initiator__last_name",
+        "initiator__first_name",
+        "initiator__patronymic",
+        "opponent__email",
+        "opponent__last_name",
+        "opponent__first_name",
+        "opponent__patronymic",
+    )
+    list_display = (
+        "initiator",
+        "opponent",
+        "start_webdial",
+        "end_webdial",
+        "webdial_duration",
+        "end_webdial_reason",
+    )
+    list_filter = ("webdial_happen", "end_webdial_reason")
--- propacienta/webdials/models.py
+++ propacienta/webdials/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth import get_user_model
+# from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class WebDial(models.Model):
+    """Users webdial model."""
+    END_WEBDIAL_REASONS = [
+        ("initiator", _("Инициатор")),
+        ("opponent", _("Вызываемый")),
+        ("error", _("Ошибка")),
+        ("opponent_reject", _("Отбой")),
+    ]
+    uuid = models.UUIDField("Идентификатор")
+    initiator = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name=_("Инициатор"),
+        related_name="initiator_webdials"
+    )
+    opponent = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name=_("Вызываемый пользователь"),
+        related_name="opponent_webdials"
+    )
+    start_webdial = models.DateTimeField(_("Начало разговора"), auto_now_add=True)
+    end_webdial = models.DateTimeField(_("Окончание разговора"), blank=True, null=True)
+    webdial_duration = models.DurationField(
+        _("Продолжительность разговора"),
+        blank=True,
+        null=True
+    )
+    end_webdial_reason = models.CharField(
+        _("Причина окончания разговора"),
+        blank=True,
+        choices=END_WEBDIAL_REASONS,
+        default="error",
+        max_length=20
+        )
+    webdial_happen = models.BooleanField(
+        _("Звонок состоялся"),
+        default=False
+    )
+
+    class Meta:
+        verbose_name = "Звонок"
+        verbose_name_plural = "Звонки"
+        ordering = ["-id"]
+
+    def __str__(self) -> str:
+        return "Звонок между {} и {}".format(self.initiator.get_fio(), self.opponent.get_fio())
