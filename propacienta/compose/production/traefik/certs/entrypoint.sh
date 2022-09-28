6c74f0989da4f3cc2f57f0cffae462d81826008c
ghostforpy <ghostformobile@gmail.com>
2022-09-28 15:16:57 +0300

    .

--- propacienta/compose/production/traefik/certs/entrypoint.sh
+++ propacienta/compose/production/traefik/certs/entrypoint.sh
@@ -1,3 +1,4 @@
 apk add jq
 cat /data/acme.json | jq -r '.letsencrypt | .Certificates[] | .certificate' | base64 -d > /certs/cert.crt
 cat /data/acme.json | jq -r '.letsencrypt | .Certificates[] | .key' | base64 -d > /certs/key.key
+cp /data/acme.json /certs/acme.json
