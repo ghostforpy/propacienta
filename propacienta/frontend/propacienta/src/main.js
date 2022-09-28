3e523d64331b9ec663d166d5d44dd845bc0d1d54
ghostforpy <ghostformobile@gmail.com>
2022-09-28 13:51:08 +0300

    add sentry to vuejs

--- propacienta/frontend/propacienta/src/main.js
+++ propacienta/frontend/propacienta/src/main.js
@@ -5,9 +5,33 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
 
+
+
 Vue.config.productionTip = false
 Vue.use(VTooltip)
 Vue.prototype.$eventBus = new Vue();
+
+if (process.env.NODE_ENV === "production") {
+  import * as Sentry from "@sentry/vue";
+  import { BrowserTracing } from "@sentry/tracing";
+  const SENTRY_DSN = process.env.SENTRY_DSN
+  const BASE_URL = process.env.BASE_URL
+  Sentry.init({
+    Vue,
+    dsn: SENTRY_DSN,
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracingOrigins: ["localhost", BASE_URL, /^\//],
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
 
 new Vue({
   router,
