b4966aeec7b146b9537aeeeae0a54012b868bd8a
ghostforpy <ghostformobile@gmail.com>
2022-09-28 12:49:56 +0300

    .

--- propacienta/config/settings/base.py
+++ propacienta/config/settings/base.py
@@ -169,7 +169,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/api/{}/static/".format(
+STATIC_URL = "/{}/static/".format(
     env("DJANGO_ADMIN_URL", default="admin",)
 )
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
