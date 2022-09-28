e5086f6f1e1906e73671b7a0595e053734a2d4f6
ghostforpy <ghostformobile@gmail.com>
2022-09-28 12:38:49 +0300

    edit for serve admin static

--- propacienta/config/settings/base.py
+++ propacienta/config/settings/base.py
@@ -169,7 +169,9 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/static/"
+STATIC_URL = "/api/{}/static/".format(
+    env("DJANGO_ADMIN_URL", default="admin",)
+)
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
